6c56b32205c5fe11547e136cfa0ad8e5f78e10c4
ayan💁🏿‍♂️ <ayan@Ayans-MacBook-Pro.local>
2025-05-22 00:55:25 +0500

    Vertex java

---
+++ Vertex.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices = new HashMap<>();
+
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
+}
